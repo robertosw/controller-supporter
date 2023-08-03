deb43a3b01f3181328807d942a766321c204edbf
Robert Oswald <robert.oswald.osw@gmail.com>
2023-08-03 13:30:29 +0200

    targets aarch64-unkown-linux-gnu and armv7-unknown-linux-gnueabihf added

--- rust-project/src/main.rs
+++ rust-project/src/main.rs
@@ -1,3 +1,10 @@
+//  build standalone binary for raspi:  
+// Tier 1 (guaranteed to work):     cargo build --target aarch64-unkown-linux-gnu           added to project
+// Tier 2 (guaranteed to build):    cargo build --target armv7-unknown-linux-gnueabihf      added to project
+// Tier 2                           cargo build --target armv7-unknown-linux-gnueabihf      not yet added to project, has linux kernel 4.15, instead of the v3.2 the option above has
+// https://doc.rust-lang.org/nightly/rustc/platform-support.html
+// Raspi Zero 2W has a 64-bit Arm Cortex-A53, which is ARMv8 64bit.. so the Tier 1 option should work
+
 fn main() {
     println!("Hello, world!");
 }
