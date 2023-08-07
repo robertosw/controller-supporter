2155fed9dc57e17bdbe299829ebbe38e16508879
Robert Oswald <robert.oswald.osw@gmail.com>
2023-08-07 20:07:24 +0200

    Code cleanup

--- usb-middleman/src/main.rs
+++ usb-middleman/src/main.rs
@@ -2,9 +2,12 @@
 
 mod hidapi;
 mod rusb;
-use crate::{rusb::*, hidapi::*};
+use crate::{hidapi::*, rusb::*};
 
 fn main() {
     hidapi();
     // _rusb();
 }
+
+// Terminal 1: .../controller-supporter/usb-middleman$                  clear && cargo build --release
+// Terminal 2: .../controller-supporter/usb-middleman/target/release$   clear && sudo chown root usb-middleman && sudo chmod u+s usb-middleman && ./usb-middleman
