c07409594467c821d0afe439e2baea1c245e2ce8
Robert Oswald <robert.oswald.osw@gmail.com>
2023-08-09 23:29:17 +0200

    Refined Dockerfile and LaunchFile to copy built binary to host

--- gamepad-bridge/docker_build_and_run.sh
+++ gamepad-bridge/docker_build_and_run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-echo "building for linux/arm64"
+clear
+echo "building image for linux/arm64"
 echo
 docker build --platform linux/arm64 -t rustarm64 .
 
 echo
 echo "running container for linux/arm64"
 echo
-docker run --platform linux/arm64 -it --rm  rustarm64
+
+docker run -it --platform linux/arm64 --name rustcont rustarm64
+mkdir ./aarch64build/
+docker cp rustcont:/gamepad-bridge/target/release/gamepad-bridge ./aarch64build/release/gamepad-bridge
+docker cp rustcont:/gamepad-bridge/target/debug/gamepad-bridge ./aarch64build/debug/gamepad-bridge
+docker rm rustcont
+
