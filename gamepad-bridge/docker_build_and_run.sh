3a9429702aabf9a787dd3257dd584a6f5bee00a1
Robert Oswald <robert.oswald.osw@gmail.com>
2023-08-09 23:31:56 +0200

    added some used links incase docker-based solution for crosscompile doesnt work

--- gamepad-bridge/docker_build_and_run.sh
+++ gamepad-bridge/docker_build_and_run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Random collection of might-be useful links if this ever does not work
+# https://www.modio.se/cross-compiling-rust-binaries-to-armv7.html
+# https://kerkour.com/rust-cross-compilation
+# https://github.com/cross-rs/cross/tree/main
+
 clear
 echo "building image for linux/arm64"
 echo
@@ -13,4 +19,3 @@ mkdir ./aarch64build/
 docker cp rustcont:/gamepad-bridge/target/release/gamepad-bridge ./aarch64build/release/gamepad-bridge
 docker cp rustcont:/gamepad-bridge/target/debug/gamepad-bridge ./aarch64build/debug/gamepad-bridge
 docker rm rustcont
-
