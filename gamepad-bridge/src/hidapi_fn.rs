bc46da73b9ab981bc8ddea436c67f902953f8a6a
Robert Oswald <robert.oswald.osw@gmail.com>
2023-08-10 15:39:04 +0200

    restructured files for clear logical flow

---
+++ gamepad-bridge/src/hidapi_fn.rs
@@ -0,0 +1,76 @@
+use ::hidapi::{BusType, HidApi, HidDevice};
+use hidapi::DeviceInfo;
+use std::process::exit;
+
+use crate::hidapi_read_ps5_usb::*;
+
+// TODO BT connection and scanning
+
+/// Returns all supported gamepads, might be 0 <br>
+/// If no gamepads are connected, displays this in terminal and exits program
+pub fn find_supported_gamepads(api: HidApi) -> Vec<DeviceInfo> {
+    // since api.device_list() returns an iterator,
+    // it would be nececarry to go trough the whole
+    // iterator twice to get the length and than handle all devices.
+    // This way, only once is nececarry
+    let mut device_count: usize = 0;
+    let mut gamepads: Vec<hidapi::DeviceInfo> = Vec::new();
+
+    // Print out information about all connected devices
+    for device_info in api.device_list() {
+        device_count += 1;
+        let vendor_id: u16 = device_info.vendor_id();
+        let bus_type: BusType = device_info.bus_type();
+
+        // Check if device is supported
+        match bus_type {
+            BusType::Usb => {
+                if vendor_id != 1356 {
+                    // Sony is 1356
+                    continue;
+                }
+            }
+            BusType::Bluetooth => {
+                // TODO Rewrite function below to support BT and both gamepads
+                println!("Bluetooth is not yet supported");
+                println!("Device name {:?}", device_info.product_string());
+                continue;
+            }
+            _ => continue,
+        };
+
+        println!("bus type {:?}", device_info.bus_type());
+        println!("interface nr {:?}", device_info.interface_number());
+        println!("product {:?}", device_info.product_string());
+        println!("release {:?}", device_info.release_number());
+        println!("usage {:?}", device_info.usage());
+        println!("usage page {:?}", device_info.usage_page());
+
+        println!("{:#?}", device_info);
+
+        gamepads.push(device_info.to_owned());
+    }
+
+    // No devices connected
+    if device_count == 0 {
+        println!("No devices detected by HidApi. Exiting program.");
+        exit(1);
+    }
+
+    return gamepads;
+}
+
+// Terminal 1: .../gamepad-bridge/gamepad-bridge$                  clear && cargo build --release
+// Terminal 2: .../gamepad-bridge/gamepad-bridge/target/release$   clear && sudo chown root gamepad-bridge && sudo chmod u+s gamepad-bridge && ./gamepad-bridge
+
+pub fn open_device(device_info: DeviceInfo, api: HidApi) {
+    let vid: u16 = 1356;
+    let pid: u16 = device_info.product_id();
+
+    let open_device = match api.open(vid, pid) {
+        Ok(hid_device) => hid_device,
+        Err(err) => panic!("Error: {:?}", err),
+    };
+
+    read_ps5_usb(&open_device);
+}
--- gamepad-bridge/src/main.rs
+++ gamepad-bridge/src/main.rs
@@ -1,63 +1,23 @@
 #![allow(unused_imports, dead_code)]
 
-mod read_ps5_usb;
-mod structs;
+use hidapi::HidApi;
 use std::process::exit;
 
-use ::hidapi::{BusType, HidApi, HidDevice};
-
-use crate::read_ps5_usb::*;
-
-// TODO BT connection and scanning
+mod hidapi_fn;
+mod hidapi_structs;
+mod hidapi_read_ps5_usb;
+use crate::hidapi_fn::find_supported_gamepads;
 
 fn main() {
-    println!("started");
+    println!("\n Gamepad-Bridge started");
 
-    // TODO Handle this correctly, the program ends here on raspi
-    let api = HidApi::new().unwrap();
+    let api = match HidApi::new() {
+        Ok(api) => api,
+        Err(err) => {
+            println!("Error getting HidApi access: {:?}", err);
+            exit(2);
+        }
+    };
 
-    // Print out information about all connected devices
-    for device in api.device_list() {
-        let vendor_id: u16 = device.vendor_id();
-        let bus_type: BusType = device.bus_type();
-
-        // Check if device is supported
-        match bus_type {
-            BusType::Usb => {
-                if vendor_id != 1356 {
-                    // Sony is 1356
-                    continue;
-                }
-            }
-            BusType::Bluetooth => {
-                // TODO Rewrite function below to support BT and both gamepads
-                println!("Bluetooth is not yet supported");
-                println!("Device name {:?}", device.product_string());
-                continue;
-            }
-            _ => continue,
-        };
-
-        println!("bus type {:?}", device.bus_type());
-        println!("interface nr {:?}", device.interface_number());
-        println!("product {:?}", device.product_string());
-        println!("release {:?}", device.release_number());
-        println!("usage {:?}", device.usage());
-        println!("usage page {:?}", device.usage_page());
-
-        println!("{:#?}", device);
-
-        let vid: u16 = 1356;
-        let pid: u16 = device.product_id();
-
-        let open_device = match api.open(vid, pid) {
-            Ok(hid_device) => hid_device,
-            Err(err) => panic!("Error: {:?}", err),
-        };
-
-        read_ps5_usb(&open_device);
-    }
+    let gamepads: Vec<hidapi::DeviceInfo> = find_supported_gamepads(api);
 }
-
-// Terminal 1: .../gamepad-bridge/gamepad-bridge$                  clear && cargo build --release
-// Terminal 2: .../gamepad-bridge/gamepad-bridge/target/release$   clear && sudo chown root gamepad-bridge && sudo chmod u+s gamepad-bridge && ./gamepad-bridge
