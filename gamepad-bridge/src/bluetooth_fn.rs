025ec3ddd732ef160febd6ecbe9026f293a19731
Robert Oswald <robert.oswald.osw@gmail.com>
2023-08-18 22:20:13 +0200

    moved bluetooth functions into bluetooth file

--- gamepad-bridge/src/bluetooth_fn.rs
+++ gamepad-bridge/src/bluetooth_fn.rs
@@ -34,6 +34,29 @@ use std::{
 //     trust <mac>     trust this gamepad, so that it will be connected automatically in the future
 //     pairable off
 
+/// turns bluetooth on
+pub fn bt_power_on() {
+    let output_power_on = match Command::new("bluetoothctl").args(["power", "on"]).output() {
+        Ok(out) => out,
+        Err(err) => {
+            println!("unwrapping the output failed: {:?}", err);
+            exit(1);
+        }
+    };
+
+    let stdout = String::from_utf8(output_power_on.stdout).ok();
+    let stderr = String::from_utf8(output_power_on.stderr).ok();
+
+    if !output_power_on.status.success() {
+        println!("bluetoothctl power on failed:");
+        println!("{:?}", stderr);
+        exit(1);
+    }
+
+    println!("Stdout: {:?}", stdout);
+    println!("Stderr: {:?}", stderr);
+}
+
 /// calling `thread_handle.join()` on this thread terminates the  `bluetoothctl scan on` command
 /// so the for loop inside this function is left and the function return smoothly
 pub fn bt_scan_on_threaded() -> (Arc<Mutex<Vec<String>>>, thread::JoinHandle<()>) {
@@ -41,14 +64,96 @@ pub fn bt_scan_on_threaded() -> (Arc<Mutex<Vec<String>>>, thread::JoinHandle<()>
 
     // spawn new thread
     let scan_clone = scan_output.clone();
-    let handle = thread::spawn(move || bt_scan_on_thread(scan_clone));
+    let handle = thread::spawn(move || _bt_scan_on_thread(scan_clone));
 
     return (scan_output, handle);
 }
 
+/// Scan the given output of `bluetoothctl scan on` for yet unknown gamepads
+pub fn handle_bt_scan_output(shared_mem_scan_output: &Arc<Mutex<Vec<String>>>) {
+    let output_copy: Vec<String> = _move_from_shared_mem(shared_mem_scan_output);
+
+    // check if anything new was added and do something with it
+    for line in output_copy {
+        let line_str: &str = line.as_str();
+
+        if line.contains("Discovery started") {
+            // First line of this command can be ignored
+            continue;
+        }
+
+        let first_asci_upper: usize = match line.find(|c: char| c.is_ascii_uppercase()) {
+            None => continue,
+            Some(usize) => usize,
+        };
+        let log_type = &line_str[first_asci_upper..first_asci_upper + 3];
+        println!("Type: {:?}", log_type);
+
+        match log_type {
+            "NEW" => {
+                if _is_device_controller(line_str) == true {
+                    // TODO Connect to controller
+                    return;
+                }
+            }
+            "CHG" => (),
+            "DEL" => (),
+            _ => (),
+        }
+    }
+}
+
+/// After this function returns the device has been handled, so the loop can be continued
+fn _is_device_controller(output_line: &str) -> bool {
+    // Possible outputs
+    // [NEW] Device 54:C2:8B:53:A4:3C 54-C2-8B-53-A4-3C         --> irrelevant
+    // [NEW] Device 54:C2:8B:53:A4:3C Name of Device            --> THIS is interesting
+    // [CHG] Controller 14:F6:D8:7D:51:94 Discovering: yes      --> everything that starts with Controller can be discharged
+    // [CHG] Device 6E:FF:68:D4:4D:CC RSSI: -92                 --> irrelevant
+    // [CHG] Device 6E:FF:68:D4:4D:CC TxPower: 17               --> irrelevant
+    // [DEL]
+
+    // the contents of [] are a bit messed up because of the colors:
+    // [\u{1}\u{1b}[0;93m\u{2}CHG\u{1}\u{1b}[0m\u{2}]
+    // [\u{1}\u{1b}[0;92m\u{2}NEW\u{1}\u{1b}[0m\u{2}]
+
+    // Cut off the log type
+    let index_next_whitespace: usize = match output_line.find(|c: char| c.is_whitespace()) {
+        None => return false,
+        Some(usize) => usize,
+    };
+    let line_str = &output_line[index_next_whitespace + 1..];
+
+    // get the descriptor and cut it off
+    let (descriptor, line_str) = match line_str.split_once(char::is_whitespace) {
+        Some((extracted, remainder)) => (extracted, remainder),
+        None => return false,
+    };
+    println!("descriptor: {:?}", &descriptor);
+    if descriptor != "Device" {
+        println!("");
+        return false;
+    }
+
+    // get mac address and device name
+    let (mac_address, device_name) = match line_str.split_once(char::is_whitespace) {
+        Some((extracted, remainder)) => (extracted, remainder),
+        None => return false,
+    };
+    println!("mac: {:?}", &mac_address);
+    println!("device_name: {:?}", &device_name);
+    println!("");
+
+    if device_name.contains(" controller") || device_name.contains(" Controller") {
+        return true;
+    }
+
+    return false;
+}
+
 /// turn bluetoothctl scanning on and write output without buffering into file with param `output_file_name` <br>
 /// If no error is occured, function never ends. Should be run in seperate thread
-fn bt_scan_on_thread(scan_output: Arc<Mutex<Vec<String>>>) {
+fn _bt_scan_on_thread(scan_output: Arc<Mutex<Vec<String>>>) {
     // start scanning for devices
     let child_cmd = match Command::new("stdbuf")
         .args(["-o0", "bluetoothctl", "scan", "on"])
@@ -101,25 +206,16 @@ fn bt_scan_on_thread(scan_output: Arc<Mutex<Vec<String>>>) {
     // println!("{:?}", scan_off);
 }
 
-/// turns bluetooth on
-pub fn bt_power_on() {
-    let output_power_on = match Command::new("bluetoothctl").args(["power", "on"]).output() {
-        Ok(out) => out,
-        Err(err) => {
-            println!("unwrapping the output failed: {:?}", err);
-            exit(1);
-        }
-    };
+/// returns the current contents of the shared memory, clears shared memory in the process
+fn _move_from_shared_mem(shared_memory: &Arc<Mutex<Vec<String>>>) -> Vec<String> {
+    // always unwrap after calling lock.
+    // If lock fails, this thread should panic because the other thread is in a deadlock
+    // If the Mutex is locked by other thread, this one waits here until free
+    let mut scan_output_locked = shared_memory.lock().unwrap();
+    let copy: Vec<String> = scan_output_locked.clone();
+    scan_output_locked.clear();
 
-    let stdout = String::from_utf8(output_power_on.stdout).ok();
-    let stderr = String::from_utf8(output_power_on.stderr).ok();
+    return copy;
 
-    if !output_power_on.status.success() {
-        println!("bluetoothctl power on failed:");
-        println!("{:?}", stderr);
-        exit(1);
-    }
-
-    println!("Stdout: {:?}", stdout);
-    println!("Stderr: {:?}", stderr);
+    // locks are released after a block goes out of sope
 }
--- gamepad-bridge/src/main.rs
+++ gamepad-bridge/src/main.rs
@@ -52,31 +52,7 @@ fn main() {
     // find new controllers
     // loop while ctrlc has not been pressed (.load == true)
     while ctrlc.load(Ordering::SeqCst) {
-        let output_copy: Vec<String> = move_from_shared_mem(&shared_mem_scan_output);
-
-        // check if anything new was added and do something with it
-        for line in output_copy {
-            let line_str: &str = line.as_str();
-
-            if line.contains("Discovery started") {
-                // First line of this command can be ignored
-                continue;
-            }
-
-            let first_asci_upper: usize = match line.find(|c: char| c.is_ascii_uppercase()) {
-                None => continue,
-                Some(usize) => usize,
-            };
-            let log_type = &line_str[first_asci_upper..first_asci_upper + 3];
-            println!("Type: {:?}", log_type);
-
-            match log_type {
-                "NEW" => check_if_device_is_controller(line_str),
-                "CHG" => (),
-                "DEL" => (),
-                _ => (),
-            }
-        }
+        handle_bt_scan_output(&shared_mem_scan_output);
 
         thread::sleep(Duration::from_millis(500));
     }
@@ -84,66 +60,9 @@ fn main() {
     thread_handle.join().unwrap();
 }
 
-/// After this function returns the device has been handled, so the loop can be continued
-fn check_if_device_is_controller(line_str: &str) {
-    // Possible outputs
-    // [NEW] Device 54:C2:8B:53:A4:3C 54-C2-8B-53-A4-3C         --> irrelevant
-    // [NEW] Device 54:C2:8B:53:A4:3C Name of Device            --> THIS is interesting
-    // [CHG] Controller 14:F6:D8:7D:51:94 Discovering: yes      --> everything that starts with Controller can be discharged
-    // [CHG] Device 6E:FF:68:D4:4D:CC RSSI: -92                 --> irrelevant
-    // [CHG] Device 6E:FF:68:D4:4D:CC TxPower: 17               --> irrelevant
-    // [DEL]
 
-    // the contents of [] are a bit messed up because of the colors:
-    // [\u{1}\u{1b}[0;93m\u{2}CHG\u{1}\u{1b}[0m\u{2}]
-    // [\u{1}\u{1b}[0;92m\u{2}NEW\u{1}\u{1b}[0m\u{2}]
 
-    // Cut off the log type
-    let index_next_whitespace: usize = match line_str.find(|c: char| c.is_whitespace()) {
-        None => return,
-        Some(usize) => usize,
-    };
-    let line_str = &line_str[index_next_whitespace + 1..];
 
-    // get the descriptor and cut it off
-    let (descriptor, line_str) = match line_str.split_once(char::is_whitespace) {
-        Some((extracted, remainder)) => (extracted, remainder),
-        None => return,
-    };
-    println!("descriptor: {:?}", &descriptor);
-    if descriptor != "Device" {
-        println!("");
-        return;
-    }
-
-    // get mac address and device name
-    let (mac_address, device_name) = match line_str.split_once(char::is_whitespace) {
-        Some((extracted, remainder)) => (extracted, remainder),
-        None => return,
-    };
-    println!("mac: {:?}", &mac_address);
-    println!("device_name: {:?}", &device_name);
-    if device_name.contains(" controller") || device_name.contains(" Controller") {
-        // TODO Connect to controller
-    }
-
-    println!("");
-    return;
-}
-
-/// returns the current contents of the shared memory, clears shared memory in the process
-fn move_from_shared_mem(shared_memory: &Arc<Mutex<Vec<String>>>) -> Vec<String> {
-    // always unwrap after calling lock.
-    // If lock fails, this thread should panic because the other thread is in a deadlock
-    // If the Mutex is locked by other thread, this one waits here until free
-    let mut scan_output_locked = shared_memory.lock().unwrap();
-    let copy: Vec<String> = scan_output_locked.clone();
-    scan_output_locked.clear();
-
-    return copy;
-
-    // locks are released after a block goes out of sope
-}
 
 fn hidapi_starter() {
     let api = match HidApi::new() {
