2bf0aaf0bfd3401d3d072659dd88dfe7cb9392e1
Robert Oswald <robert.oswald.osw@gmail.com>
2023-09-10 20:35:33 +0200

    Removed everything

--- archive/usb-middleman/src/main.rs
+++
@@ -1,6 +0,0 @@
-// NOTE
-// This is intended to be the testing ground for the connections (BT / USB) themselves
-
-fn main() {
-
-}
--- archive/with_std/src/read_input.rs
+++
@@ -1,5 +0,0 @@
-use crate::structs::GameControllerSimple;
-
-pub fn read_controller_input(_controller: GameControllerSimple) {
-    println!("fake input read :D");
-}
