15213363a31c00dd8a24ce980a497926e784a6ae
Robert Oswald <robert.oswald.osw@gmail.com>
2023-09-15 21:40:02 +0000

    idea 1 (elapsed with integer division)

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
+use std::time::Instant;
 use usb_gadget::UsbGadgetDescriptor;
 
 mod bluetooth_fn;
@@ -48,6 +49,11 @@ fn main() {
     // ----- Enable Gadget
     // If this is done later, the host might run into errors when trying to classify this device and turn it off
     // TODO output_gamepad should be expected from a command argument or set to a default if not given
+
+    single_thread_interval(Duration::from_micros(500));
+
+    exit(0);
+
     let output_gamepad: &Gamepad = &DUALSENSE;
     // output_gamepad.gadget.configure_device();
 
@@ -87,6 +93,51 @@ fn main() {
     // TODO undo gadget config completely, so that this program can be rerun without errors from configure_device()
     thread_handle_read_input.join().unwrap();
     thread_handle_write_output.join().unwrap();
+}
+
+fn single_thread_interval(interval: Duration) {
+    // its safe to use u128 for nanoseconds
+    // 2^64 ns are ~580 years
+    // so 2^128 are 580² years
+
+    let start: Instant = Instant::now();
+    let interval_ns = interval.as_nanos();
+    let mut interval_counts_before: u128 = 0;
+
+    let mut diffs: Vec<Duration> = Vec::new();
+
+    const ROUNDS: u128 = 1000;
+
+    while interval_counts_before < ROUNDS {
+        let elapsed_ns: u128 = start.elapsed().as_nanos();
+        let interval_counts_now: u128 = elapsed_ns / interval_ns;
+
+        if interval_counts_now > interval_counts_before {
+            let now = Instant::now();
+            let expected = start + (interval * interval_counts_now as u32);
+
+            diffs.push(now - expected);
+
+
+            
+            interval_counts_before = interval_counts_now;
+        }
+    }
+
+
+    //  benchmark results
+
+    let mut avg_ns = 0;
+    let mut avg_perc: f64 = 0.0;
+
+    for difference in diffs {
+        avg_ns += difference.as_nanos();
+        let error_percent: f64 = (difference.as_nanos() as f64 / interval.as_nanos() as f64) * 100.0;
+        avg_perc += error_percent;
+    }
+
+    println!("Avg ABS  {} ns", avg_ns / ROUNDS);
+    println!("Avg PERC {:2.1?} %", avg_perc / ROUNDS as f64);
 }
 
 // Ideas for program flow
